5b6ba0513d6d10b59eaf8e81dc8eaf2157da8688
Kasey Linden <kaseylinden@gmail.com>
2019-09-22 07:40:21 -0500

    change entropy limit from 1000 to 800
    This was failing on an automated build via Packer

--- controls/os_spec.rb
+++ controls/os_spec.rb
@@ -178,9 +178,9 @@ end
 control 'os-08' do
   impact 1.0
   title 'Entropy'
-  desc 'Check system has enough entropy - greater than 1000'
+  desc 'Check system has enough entropy - greater than 800'
   describe file('/proc/sys/kernel/random/entropy_avail').content.to_i do
-    it { should >= 1000 }
+    it { should >= 800 }
   end
 end
 
